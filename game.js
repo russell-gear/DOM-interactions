727779dc6121249146157f4658906d55cfac928f
russell-gear <russellgear@gmail.com>
2019-09-25 10:22:49 +1200

    add DOM interactions

--- game.js
+++ game.js
@@ -12,8 +12,11 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
+
 
 function makeGreen (evt) {
   evt.preventDefault()
@@ -22,8 +25,16 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -33,7 +44,9 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  totals.blue = document.getElementsByClassName("blue").length;
+  totals.green = document.getElementsByClassName("green").length;
+  totals.hide = document.getElementsByClassName("invisible").length;
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
